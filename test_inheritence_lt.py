82ab727e357a68b228ee2fbe58e3d65eae0d63b5
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2018-04-13 13:59:12 +0530

    Correctly access the attribute using self

--- test_inheritence_lt.py
+++ test_inheritence_lt.py
@@ -4,7 +4,7 @@ import math
 class shape:
     def area(self):
         raise NotImplementedError
-    
+
     def __lt__(self, other):
         return self.area() < other.area()
 
@@ -22,19 +22,20 @@ class rectangle(shape):
 
     def area(self):
         return self.l * self.h
-    
+
     def __lt__(self, other):
         if self.l != other.l:
             return self.l < other.l
         else:
             return self.h < other.h
 
+
 class circle(shape):
     def __init__(self, r):
         self.r = r
 
     def area(self):
-        return (4/3)*math.pi*r*r
+        return (4/3) * math.pi * self.r * self.r
 
     def __lt__(self, other):
         return self.r < other.r
